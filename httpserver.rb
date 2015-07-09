aa98cb1f0b4fcf0d5f8267c3fd84617d5225d1d5
Dicko Sow <s12.dsow@gmail.com>
2015-07-08 23:07:00 -0400

    Can read and handle requests for static website

--- httpserver.rb
+++ httpserver.rb
@@ -1,17 +1,48 @@
-require 'socket'
+require "socket"
 
-server = TCPServer.new 2000
+server = TCPServer.new(2000)
 
-loop do
-  client = server.accept #connection establishing between client and server
-  while raw_input = client.gets  #client.gets is reading from the client
-    line = raw_input.chomp if raw_input        
+def read_request(client)
+  lines = []
+  while raw_input = client.gets
+    line = raw_input.chomp if raw_input
+    lines << line
+    p line
     if line.empty?
-      client.puts "HTTP/1.1 200 OK"
-      client.puts  ""
-      client.puts "<h1>Hooray</h1>"
-      client.close
-      break
+      x = lines.first.split(" ")
+      path = x[1].split("?")[0]
+      return {
+        verb: x[0],
+        path: path,
+        protocol: x[2]
+      }
     end
   end
+end
+
+def handle_request(request, &block)
+  response = { body: "", status: "404 Not Found" }
+  if request[:path] == "/test"
+    response[:status] = "200 OK"
+    response[:body] = "hooray!"
+  end
+  yield response
+end
+
+loop do
+  client = server.accept
+  request = read_request(client)
+  p request
+
+  handle_request(request) do |response|
+    client.puts "HTTP/1.1 #{response[:status]}"
+    client.puts "Content-Type: text/html"
+    client.puts ""
+
+    response[:body].split("\n").each do |line|
+      client.puts line
+    end
+  end
+
+  client.close
 end
