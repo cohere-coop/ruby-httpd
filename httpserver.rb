12139f63dbac59bee9af89c08d451071d8d4f491
Anna Neyzberg <aneyzberg@gmail.com>
2015-07-08 19:12:24 -0700

    return static website working

--- httpserver.rb
+++ httpserver.rb
@@ -4,9 +4,12 @@ server = TCPServer.new 2000
 
 loop do
   client = server.accept #connection establishing between client and server
-  while line = client.gets.chomp  #client.gets is reading from the client
-    p line
-    if line == "exit"
+  while raw_input = client.gets  #client.gets is reading from the client
+    line = raw_input.chomp if raw_input        
+    if line.empty?
+      client.puts "HTTP/1.1 200 OK"
+      client.puts  ""
+      client.puts "<h1>Hooray</h1>"
       client.close
       break
     end
